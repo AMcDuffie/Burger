12d6701d7e8b2ab2b620e99c98169a6b31509f64
Arlette McDuffie <38673010+AMcDuffie@users.noreply.github.com>
2019-03-11 07:15:49 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 -- Insert data into burger table
-INSERT INTO burger (burger_name, devoured) VALUES ("Chilli Cheese Burger", 0);
-INSERT INTO burger (burger_name, devoured) VALUES ("Ham & Turkey Burger", 0);
-INSERT INTO burger (burger_name, devoured) VALUES ("Mushroom & Swiss Double Turkey Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chilli Cheese Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ham & Turkey Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom & Swiss Double Turkey Burger", 0);
